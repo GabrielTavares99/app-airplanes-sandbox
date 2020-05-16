1c6349fa2ded5b8670fff65e23f17c75bc828303
Gabriel Tavares <gabriel.silva@documentodoestudante.com.br>
2020-05-16 17:40:59 -0300

    widget text

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,14 @@ class BarberShop extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //remove debugger banner
-      theme: ThemeData(
-        primaryColor: Colors.red
-      ),
+      theme: ThemeData(primaryColor: Colors.red),
       home: HomePage(),
     );
   }
 
-  home(){
+  home() {
     return Scaffold(
-      appBar: AppBar(
-          title: Text("Baby Barbershop")
-      ),
+      appBar: AppBar(title: Text("Baby Barbershop")),
       body: Container(
         color: Colors.lightBlue,
       ),
@@ -32,13 +28,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text("Baby Barbershop")
-      ),
+      appBar: AppBar(title: Text("Baby Barbershop")),
       body: Container(
         color: Colors.green,
+        child: Center(
+          child: Text(
+            "Bem vindo!",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.amber,
+              decorationStyle: TextDecorationStyle.wavy,
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-
