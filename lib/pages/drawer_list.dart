f6fa1278a818e5d64677d612daf1c3c70e84b1a4
Gabriel Tavares <gabriel.silva@documentodoestudante.com.br>
2020-05-19 09:14:05 -0300

    drawer: creating a safe area

--- lib/pages/drawer_list.dart
+++ lib/pages/drawer_list.dart
@@ -3,39 +3,41 @@ import 'package:flutter/material.dart';
 class DrawerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: Text("Anonymous"),
-            accountEmail: Text("anonymous@gmail.com"),
-            currentAccountPicture: CircleAvatar(
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Anonymous"),
+              accountEmail: Text("anonymous@gmail.com"),
+              currentAccountPicture: CircleAvatar(
 //              backgroundImage: AssetImage("assets/images/hair/airplane1.jpg"),
-              backgroundImage: NetworkImage(
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQu9W74tCsvU8cGdGSU5wlgoDhmyn1L6v3Hk44FTjwTIzrTxBdV&usqp=CAU"),
+                backgroundImage: NetworkImage(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQu9W74tCsvU8cGdGSU5wlgoDhmyn1L6v3Hk44FTjwTIzrTxBdV&usqp=CAU"),
+              ),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.airplanemode_active),
-            title: Text("Militar"),
-            subtitle: Text("Militar airplanes"),
-            trailing: Icon(Icons.arrow_right),
-            onTap: () {
-              print("ITEM 1");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.airplanemode_active),
-            title: Text("Civil"),
-            subtitle: Text("Civil airplanes"),
-            trailing: Icon(Icons.arrow_right),
-            onTap: () {
-              print("ITEM 2");
-              Navigator.pop(context);
-            },
-          ),
-        ],
+            ListTile(
+              leading: Icon(Icons.airplanemode_active),
+              title: Text("Militar"),
+              subtitle: Text("Militar airplanes"),
+              trailing: Icon(Icons.arrow_right),
+              onTap: () {
+                print("ITEM 1");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.airplanemode_active),
+              title: Text("Civil"),
+              subtitle: Text("Civil airplanes"),
+              trailing: Icon(Icons.arrow_right),
+              onTap: () {
+                print("ITEM 2");
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
