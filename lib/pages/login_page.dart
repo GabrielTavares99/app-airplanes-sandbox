e0edabea637b78c838232a58798d11fe8e61b7b6
Gabriel Tavares <gabriel.silva@documentodoestudante.com.br>
2020-05-19 22:57:50 -0300

    form: adding custom validation

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,18 @@
+import 'package:app_master_airplanes/pages/home_page.dart';
+import 'package:app_master_airplanes/utils/nav_util.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final _tUsername = TextEditingController();
+
   final _tPassword = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -16,31 +26,40 @@ class LoginPage extends StatelessWidget {
   }
 
   _body(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.all(16),
-      children: <Widget>[
-        _formField(
-          "Username",
-          "Type your username",
-          controller: _tUsername,
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        _formField(
-          "Password",
-          "Type your password",
-          controller: _tPassword,
-          obscureContent: true,
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        _buttonLogin(
-          _onClickLogin,
-        ),
-      ],
+    return Form(
+      key: _formKey,
+      child: ListView(
+        padding: EdgeInsets.all(16),
+        children: <Widget>[
+          _formField(
+            "Username",
+            "Type your username",
+            controller: _tUsername,
+            validator: _validateUsername,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          _formField(
+            "Password",
+            "Type your password",
+            controller: _tPassword,
+            obscureContent: true,
+            validator: _validatePassword,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          _buttonLogin(
+            _onClickLogin,
+          ),
+        ],
+      ),
     );
+  }
+
+  String _validateUsername(String text) {
+    return text.isEmpty ? "Field must not be empty..." : null;
   }
 
   _buttonLogin(Function onPressed) {
@@ -53,11 +72,17 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  TextFormField _formField(String label, hint,
-      {controller, bool obscureContent: false}) {
+  TextFormField _formField(
+    String label,
+    hint, {
+    controller,
+    bool obscureContent: false,
+    FormFieldValidator<String> validator,
+  }) {
     return TextFormField(
       controller: controller,
       obscureText: obscureContent,
+      validator: validator,
       style: TextStyle(
         color: Colors.grey,
       ),
@@ -77,8 +102,16 @@ class LoginPage extends StatelessWidget {
   }
 
   _onClickLogin() {
+    if (!_formKey.currentState.validate()) return;
     String username = _tUsername.text;
     String password = _tPassword.text;
     print("USERNAME:: $username PASSWORD:: $password");
+    push(context, HomePage());
+  }
+
+  String _validatePassword(String text) {
+    if (text.isEmpty) return "Password must not be empty...";
+    if (text.length < 5) return "Password must contain more than 5 characters";
+    return null;
   }
 }
--- lib/utils/nav_util.dart
+++ lib/utils/nav_util.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 Future push(BuildContext context, Widget page) {
-  return Navigator.push(context,
-      MaterialPageRoute(builder: (BuildContext context) {
+  return Navigator.push(context, MaterialPageRoute(builder: (
+    BuildContext context,
+  ) {
     return page;
   }));
 }
