959c950a248eb04a6dce53432c68d9df4e79811e
Gabriel Tavares <gabriel.silva@documentodoestudante.com.br>
2020-05-23 11:56:02 -0300

    http: parsing list json

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -2,8 +2,14 @@ class User {
   String username;
   num id;
   String token;
+  List<String> roles;
 
   User.fromJson(Map<String, dynamic> map)
       : username = map["username"],
-        id = map["id"];
+        id = map["id"],
+        roles = map["roles"] != null ? getRoles(map) : null;
+
+  static List<String> getRoles(Map<String, dynamic> map) {
+    return map["roles"].map<String>((role) => role.toString()).toList();
+  }
 }
