e30e7b79f3afa6daa6c81704ba7cf5cd233dc3ca
Gabriel Tavares <gabriel.silva@documentodoestudante.com.br>
2020-05-23 12:31:23 -0300

    http: treating error

--- lib/api/login_api.dart
+++ lib/api/login_api.dart
@@ -6,22 +6,27 @@ import 'package:http/http.dart' as http;
 
 class LoginApi {
   static Future<ApiResponse<User>> login(username, password) async {
-    var url = 'http://192.168.100.22:8080/login';
-    var params = {'username': username, 'password': password};
-    var headers = {"Content-Type": "application/json"};
-    var body = json.encode(params);
-    var response = await http.post(
-      url,
-      headers: headers,
-      body: body,
-    );
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-    Map mapResponse = json.decode(response.body);
+    try {
+      var url = 'http://192.168.100.22:8080/login';
+      var params = {'username': username, 'password': password};
+      var headers = {"Content-Type": "application/json"};
+      var body = json.encode(params);
+      var response = await http.post(
+        url,
+        headers: headers,
+        body: body,
+      );
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      Map mapResponse = json.decode(response.body);
 
-    if (response.statusCode == 401)
-      return ApiResponse.error(mapResponse["message"]);
-    var user = User.fromJson(mapResponse);
-    return ApiResponse.ok(user);
+      if (response.statusCode == 401)
+        return ApiResponse.error(mapResponse["message"]);
+      var user = User.fromJson(mapResponse);
+      return ApiResponse.ok(user);
+    } catch (error, exception) {
+      print("ERROR TO LOGIN $error > $exception");
+    }
+    return ApiResponse.error("Error to login... Try after");
   }
 }
